764ca7bb03d9d294d1b380da05e6160ae710d2db
mohamadsuhan <143632014+mohamadsuhan@users.noreply.github.com>
2026-02-28 11:49:09 +0530

    Initial commit: RevConnect project setup

---
+++ src/main/java/org/example/revconnectapp/RevConnectAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.revconnectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevConnectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevConnectAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/revconnectapp/RevConnectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.revconnectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevConnectAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
